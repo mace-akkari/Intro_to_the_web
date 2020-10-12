51ca176802facabdb74a9db1da36a4acb7d25668
Mace <maceplus@gmail.com>
2020-10-12 16:26:37 +0100

    add instance variable

--- app.rb
+++ app.rb
@@ -5,5 +5,10 @@ get '/' do
 end
 
 get '/secret' do
-  'This is a secret page'
+  'This is a super super secret page'
+end
+
+get '/cat' do
+  @names = ["Amigo", "Oscar", "Viking"].sample
+  erb :index
 end
